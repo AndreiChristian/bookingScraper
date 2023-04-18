4ee5f3658d9080eb475e3835ef2543bdfe85bfc4
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-18 15:09:24 +0200

    started working on the schema

--- GROUP/UTILS/testScraper.py
+++ GROUP/UTILS/testScraper.py
@@ -1,3 +1,50 @@
+# import requests
+# from bs4 import BeautifulSoup
+# import time
+# import csv
+# from datetime import datetime
+
+# now = datetime.now()  # get the current date and time
+# # format the date and time as a string
+# time_str = now.strftime("%Y-%m-%d %H:%M:%S")
+
+
+# headers = {
+#     "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"}
+
+
+# def getUrl(hotel_url):
+#     return f"https://www.booking.com/hotel/ch/{hotel_url}.en-gb.html?checkin=2023-04-18&checkout=2023-04-19&group_adults=2&group_children=0&no_rooms=1&selected_currency=EUR"
+
+
+# def scrapeHotels(hotels):
+
+#     print("Fetching hotels data started!")
+
+#     with open("./example.csv", mode='a', newline='') as csv_file:
+#         writer = csv.writer(csv_file)
+
+#     for hotel in hotels:
+#         response = requests.get(
+#             getUrl(hotel_url=hotel["url"]), headers=headers)
+#         soup = BeautifulSoup(response.text, "html.parser")
+
+#         tr_arr = soup.find_all("tr")
+
+#         for tr in tr_arr:
+
+#             room_name = tr.find("span", {"class": "hprt-roomtype-icon-link"})
+#             room_price = tr.find(
+#                 "span", {"class": "prco-valign-middle-helper"})
+
+#             if (room_price != None) & (room_name != None):
+#                 hotel["roomTypes"].append(room_name.text.strip())
+#                 hotel["roomPrices"].append(room_price.text[3:].strip())
+#                 writer.writerow(
+#                     [time_str, hotel["name"], room_name.text.strip(), room_price.text[3:].strip()])
+
+#         print(f"Fetched data for {hotel['name']}")
+
 import requests
 from bs4 import BeautifulSoup
 import time
@@ -21,30 +68,27 @@ def scrapeHotels(hotels):
 
     print("Fetching hotels data started!")
 
-    for hotel in hotels:
-        response = requests.get(
-            getUrl(hotel_url=hotel["url"]), headers=headers)
-        soup = BeautifulSoup(response.text, "html.parser")
+    with open("./example.csv", mode='a', newline='') as csv_file:
+        writer = csv.writer(csv_file)
 
-        tr_arr = soup.find_all("tr")
+        for hotel in hotels:
+            response = requests.get(
+                getUrl(hotel_url=hotel["url"]), headers=headers)
+            soup = BeautifulSoup(response.text, "html.parser")
 
-        for tr in tr_arr:
+            tr_arr = soup.find_all("tr")
 
-            room_name = tr.find("span", {"class": "hprt-roomtype-icon-link"})
-            room_price = tr.find(
-                "span", {"class": "prco-valign-middle-helper"})
+            for tr in tr_arr:
 
-            if (room_price != None) & (room_name != None):
-                hotel["roomTypes"].append(room_name.text.strip())
-                hotel["roomPrices"].append(room_price.text[3:].strip())
+                room_name = tr.find(
+                    "span", {"class": "hprt-roomtype-icon-link"})
+                room_price = tr.find(
+                    "span", {"class": "prco-valign-middle-helper"})
 
-        print(f"Fetched data for {hotel['name']}")
+                if (room_price != None) & (room_name != None):
+                    hotel["roomTypes"].append(room_name.text.strip())
+                    hotel["roomPrices"].append(room_price.text[3:].strip())
+                    writer.writerow(
+                        [time_str, hotel["name"], room_name.text.strip(), room_price.text[3:].strip()])
 
-        with open("./example.csv", mode='a', newline='') as csv_file:
-            writer = csv.writer(csv_file)
-            for i in range(len(hotel["roomTypes"])):
-
-                writer.writerows(
-                    [time_str, hotel["name"], hotel["roomTypes"][i-1], hotel["roomPrices"]][i-1])
-
-        print(f"Added data to csv for {hotel['name']}")
+            print(f"Fetched data for {hotel['name']}")
--- GROUP/script.py
+++ GROUP/script.py
@@ -2,31 +2,31 @@ import UTILS.testScraper
 
 hotels = [
     #   mandarin oriental
-    {"name": ["Mandarin Oriental"],
+    {"name": "Mandarin Oriental",
      "url": "mandarin-oriental-geneva", "roomTypes": [], "roomPrices":[]},
     #   four seasons
-    {"name": ["Four Seasons Geneva"],
+    {"name": "Four Seasons Geneva",
      "url": "four-seasons-geneva", "roomTypes": [], "roomPrices":[]},
     #   Hotel d'Angleterre
-    {"name": ["Hotel d'Angleterre"],
+    {"name": "Hotel d'Angleterre",
      "url": "d-angleterre", "roomTypes": [], "roomPrices":[]},
     #   intercontinental-geneve
-    {"name": ["Intercontinental Geneve"],
+    {"name": "Intercontinental Geneve",
      "url": "intercontinental-geneve", "roomTypes": [], "roomPrices":[]},
     #   de-la-paix-ritz-carlton
-    {"name": ["Ritz Carlton Hotel de la Paix"],
+    {"name": "Ritz Carlton Hotel de la Paix",
      "url": "de-la-paix-ritz-carlton", "roomTypes": [], "roomPrices":[]},
     #   Beau Rivage Geneva
-    {"name": ["Beau Rivage Geneva"],
+    {"name": "Beau Rivage Geneva",
      "url": "beaurivagegeneva", "roomTypes": [], "roomPrices":[]},
     #   Fairmont Hotel
-    {"name": ["Fairmont Hotel"], "url": "grand-kempinski-geneva",
+    {"name": "Fairmont Hotel", "url": "grand-kempinski-geneva",
         "roomTypes": [], "roomPrices":[]},
     # Hotel Metropole Geneva
-    {"name": ["Metropole Geneva"], "url": "metropolegeneve",
+    {"name": "Metropole Geneva", "url": "metropolegeneve",
         "roomTypes": [], "roomPrices":[]},
     # the-woodward
-    {"name": ["The Woodward"],
+    {"name": "The Woodward",
      "url": "the-woodward",
         "roomTypes": [], "roomPrices":[]},
 
