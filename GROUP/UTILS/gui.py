234f6421de3a5fcc2bf43f17a84e2ffcfabfd2aa
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-18 16:27:56 +0200

    added a basic gui

--- GROUP/UTILS/gui.py
+++ GROUP/UTILS/gui.py
@@ -1,29 +1,41 @@
-import PySimpleGUI as sg
+import tkinter as tk
+from tkinter import ttk
 
-sg.theme('DarkTeal9')  # Set the theme
+# Define the data as a list of tuples
+data = [
+    ("2023-04-18 16:21:00", "Mandarin Oriental", "€ 803", "€ 874", "€ 1,078", "€ 1,282"),
+    ("2023-04-18 16:21:00", "Four Seasons Geneva", "€ 1,155", "€ 1,369", "€ 1,751", "€ 3,861"),
+    ("2023-04-18 16:21:00", "Hotel d'Angleterre", "€ 721", "€ 1,078", "€ 2,251", "€ 4,799"),
+    ("2023-04-18 16:21:00", "Intercontinental Geneve", "€ 624", "€ 747", "€ 1,236", "€ 7,251"),
+    ("2023-04-18 16:21:00", "Ritz Carlton Hotel de la Paix", "€ 925", "€ 1,078", "€ 1,537", "€ 2,149"),
+    ("2023-04-18 16:21:00", "Beau Rivage Geneva", "€ 747", "€ 808", "€ 1,409", "€ 2,327"),
+    ("2023-04-18 16:21:00", "Fairmont Hotel", "€ 833", "€ 1,267", "€ 1,267", "€ 5,105")
+]
 
-# Define the layout
-layout = [[sg.Text('Enter your name:'), sg.Input(key='-NAME-')],
-          [sg.Text('Select your favorite color:'),
-           sg.Combo(['Red', 'Blue', 'Green'], key='-COLOR-')],
-          [sg.Button('Submit'), sg.Button('Cancel')]]
+# Define a function to create the GUI
+def create_gui():
+    # Create the root window
+    root = tk.Tk()
+    root.title("Hotel Room Rates")
+    
+    # Create the table
+    table = ttk.Treeview(root)
+    table["columns"] = ("standard", "superior", "junior_suite", "suite")
+    table.heading("#0", text="Timestamp")
+    table.heading("standard", text="Standard")
+    table.heading("superior", text="Superior")
+    table.heading("junior_suite", text="Junior Suite")
+    table.heading("suite", text="Suite")
+    
+    # Add the data to the table
+    for row in data:
+        table.insert("", tk.END, text=row[0], values=row[1:])
+    
+    # Pack the table
+    table.pack(expand=True, fill=tk.BOTH)
+    
+    # Start the main loop
+    root.mainloop()
 
-# Create the window
-window = sg.Window('PyStay', layout)
-
-# Event loop
-while True:
-    event, values = window.read()
-
-    # If the user closes the window or clicks the Cancel button
-    if event == sg.WIN_CLOSED or event == 'Cancel':
-        break
-
-    # If the user clicks the Submit button
-    if event == 'Submit':
-        name = values['-NAME-']
-        color = values['-COLOR-']
-        sg.popup(f'Hello, {name}! Your favorite color is {color}.')
-
-# Close the window
-window.close()
+# Call the create_gui function to display the GUI
+create_gui()
